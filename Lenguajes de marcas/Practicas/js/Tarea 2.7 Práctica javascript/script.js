c28f79cd4d8dc5694a76e339af227e0054f1b202
PedroMartz89 <pedromartinezsanchez89@gmail.com>
2024-12-01 22:55:28 +0100

    1-12/24

--- Lenguajes de marcas/Practicas/js/Tarea 2.7 Práctica javascript/script.js
+++ Lenguajes de marcas/Practicas/js/Tarea 2.7 Práctica javascript/script.js
@@ -1,17 +1,20 @@
 function agregar() {
-
     let nota = document.getElementById("nota").value;
-    let prio = document.getElementById("prioridad").value;
+    let prioridad = document.getElementById("prioridad").value;
     let tiempo = document.getElementById("tiempo").value;
 
-    let nuevaNota = '<ul id="lista-tareas">Nota: ' + nota + ' Prioridad: '+prio + ' tiempo: ' + tiempo +'</ul>';
+    let tarea = document.createElement("li");
+    tarea.className = "tarea";
+    tarea.innerHTML = `${nota} // Tiempo estimado: ${tiempo} minutos // Prioridad: ${prioridad}`;
 
-    document.getElementById("listaR").innerHTML = listaR.appendChild(nuevaNota);
-    
+    let lista = document.getElementById("lista-tareas");
+    lista.appendChild(tarea);
 }
 
-function borrar(){
+function borrar() {
+    let lista = document.getElementById("lista-tareas");
+    let tareaIndex = document.getElementById("borrar-index").value;
 
-    
-
+    let tarea = lista.children[tareaIndex - 1];
+    lista.removeChild(tarea);
 }
