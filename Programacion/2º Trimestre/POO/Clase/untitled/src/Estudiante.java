d92623ed5893f37bad4351e2a97e533c0c3aebab
PedroMartz89 <pedromartinezsanchez89@gmail.com>
2025-01-14 16:45:58 +0100

    POO-Ejercicios

---
+++ Programacion/2º Trimestre/POO/Clase/untitled/src/Estudiante.java
@@ -0,0 +1,116 @@
+public class Estudiante {
+
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private int curso;
+    private int grado;
+    private String universidad;
+    private String correo;
+    private int telefono;
+
+    private static int numEstudiantes = 0;
+
+
+    public static int obtenerNumEstudiantes() {
+
+        return numEstudiantes;
+    }
+
+    public Estudiante(String nombre, String apellido1, String apellido2, int curso, int grado, String universidad, String correo, int telefono) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.curso = curso;
+        this.grado = grado;
+        this.universidad = universidad;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String nombre, String apellido1, String apellido2) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getCurso() {
+        return curso;
+    }
+
+    public void setCurso(int curso) {
+        this.curso = curso;
+    }
+
+    public int getGrado() {
+        return grado;
+    }
+
+    public void setGrado(int grado) {
+        this.grado = grado;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUniversidad() {
+        return universidad;
+    }
+
+    public void setUniversidad(String universidad) {
+        this.universidad = universidad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String infoEstudiante() {
+
+        return "Nombre: " +
+                this.nombre + "\nApellidos: "
+                + this.apellido1+ " "
+                + this.apellido2 + "\nTeléfono: "
+                + this.telefono + "\n Curso: "
+                + this.curso + "\n Grado: "
+                + this.grado + "\nUniversidad: "
+                + this.universidad
+                + "\nCorreo Electrónico: " + this.correo;
+    }
+}
---
+++ Programacion/2º Trimestre/POO/Clase/untitled/src/EstudianteTest.java
@@ -0,0 +1,84 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstudianteTest {
+    public static void main(String[] args) {
+        Estudiante alumno1 = new Estudiante();
+        alumno1.setNombre("Pedro");
+        alumno1.setApellido1("Martínez");
+        alumno1.setApellido2("Sánchez");
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    void obtenerNumEstudiantes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNombre() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNombre() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getApellido1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setApellido1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getApellido2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setApellido2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurso() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurso() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGrado() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setGrado() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCorreo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCorreo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUniversidad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setUniversidad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTelefono() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTelefono() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void infoEstudiante() {
+    }
+}
